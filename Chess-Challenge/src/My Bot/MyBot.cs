af1ce593196fc4a8cd85ac1ee4672c8529933f87
Binto86 <kuba.binter@gmail.com>
2023-07-21 17:33:33 +0200

    Start

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,6 +5,38 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        bool isWhite = board.IsWhiteToMove;
+        var bestMove = moves[0];
+        var bestEval = -100000;
+        foreach (var move in moves)
+        {
+            board.MakeMove(move);
+            var eval = Evaluation(board, isWhite);
+            if (eval > bestEval)
+            {
+                bestEval = eval;
+                bestMove = move;
+            }
+            board.UndoMove(move);
+        }
+        return bestMove;
+    }
+
+    int Evaluation(Board board, bool isWhite)
+    {
+        var result = 0;
+        result += board.GetPieceList(PieceType.Queen, true).Count * 9;
+        result += board.GetPieceList(PieceType.Rook, true).Count * 5;
+        result += board.GetPieceList(PieceType.Bishop, true).Count * 3;
+        result += board.GetPieceList(PieceType.Knight, true).Count * 3;
+        result += board.GetPieceList(PieceType.Pawn, true).Count;
+
+        result -= board.GetPieceList(PieceType.Queen, false).Count * 9;
+        result -= board.GetPieceList(PieceType.Rook, false).Count * 5;
+        result -= board.GetPieceList(PieceType.Bishop, false).Count * 3;
+        result -= board.GetPieceList(PieceType.Knight, false).Count * 3;
+        result -= board.GetPieceList(PieceType.Pawn, false).Count;
+
+        return isWhite ? result : -result;
     }
 }
